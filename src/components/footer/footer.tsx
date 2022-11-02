0ea71cd0c8c5f9576c009383419498711aa43564
Robert <rjsf06@gmail.com>
2022-11-02 09:30:50 -0300

    :sparkles: feat: making footer structure

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,20 @@
+import * as S from "./footerStyle"
+
 export default function Footer(){
     return(
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <S.footer>
+            <S.footer__content>
+                <S.footer__title >Don't miss what's happening</S.footer__title>
+                <S.footer__paragraph>People on Twitter are the first to know.</S.footer__paragraph>
+            </S.footer__content>
+            <S.footer__btn>
+                <S.footer__link href="error_404" >
+                    <S.btn__login type="button" >Log in</S.btn__login>
+                </S.footer__link>
+                <S.footer__link href="error_404">
+                    <S.btn__sign type="button" >Sign up</S.btn__sign>
+                </S.footer__link>
+            </S.footer__btn>
+        </S.footer>
     )
 }
