6c50b3b3fdb53f73a19b63161623f56200db4520
Robert <rjsf06@gmail.com>
2022-11-03 11:04:41 -0300

    :sparkles: feat: removing component main to be rendered in router-dom not aside-left

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import Left from "./components/aside-left/left";
 import Right from "./components/aside-right/right";
 import Footer from "./components/footer/footer";
-import Main from "./components/main/main";
 import * as S from "./appStyle"
+
 export default function App() {
   return (
     <S.Div>
       <Left/>
       <Right/>
-      <Main/>
       <Footer/>
     </S.Div>
   )
