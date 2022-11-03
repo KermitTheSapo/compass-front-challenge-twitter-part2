88902f86af60fc34f33a7c444a763d2079ddde13
Robert <rjsf06@gmail.com>
2022-11-03 10:05:40 -0300

    :sparkles: feat: making the input clear, time the posts and add post

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -12,11 +12,32 @@ import comment from "../../assets/imgs/main/comment.svg"
 import retweet from "../../assets/imgs/main/retweet.svg"
 import likeDisabled from "../../assets/imgs/main/like-disabled.svg"
 import share from "../../assets/imgs/main/share.svg"
-
+import { useState } from "react"
 
 
 export default function Main(){
+    const [tweet, setTweet] = useState([{
+        time: "aaaaaaa",
+        content: "aaaaaaaaaaa"
+    }])
+    let now = new Date
+
+    
+    const [text, setText] = useState("")
+
+    function add(){
+        setTweet([...tweet, { time: `${now.getHours()}h${now.getMinutes()}m`, content: text}]),
+        setText("")
+    }
+    // @ts-ignore
+    function Listener(e){
+        setText(e.target.value)
+    }
+    
+
+        
     return(
+        
         <S.Main>
             <S.main__header>
                 <S.main__header__title>Home</S.main__header__title>
@@ -27,13 +48,11 @@ export default function Main(){
                 <S.post__form action="" method="post">
                     <S.post__input >
                         <S.input__img src={profile} alt="A profile picture of a person with a black shirt and hair on their face"/>
-                        <S.input__text type="text" placeholder="What's happening?"/>
-                        {/*  minlength="1" maxlength="380" */}
+                        <S.input__text value={text} onChange={(e) => Listener(e)} type="text" placeholder="What's happening?" minLength={2} maxLength={380} />
                     </S.post__input>
                     <S.post__emoji>
                         <S.emoji__div>
-                            <S.label>
-                                {/* for="upload" */}
+                            <S.label htmlFor="upload">
                                 <S.LabelImg src={imgIcon} alt="Blue icon of a landscape and sun related to image upload"/>
                             </S.label>
                             <S.upload__img id="upload" type="file" name="file" accept="image/png, image/jpeg" multiple />
@@ -42,34 +61,40 @@ export default function Main(){
                             <S.icons src={emojiIcon} alt="Happy face icon"/>
                             <S.icons src={CalendarIcon} alt="Calendar icon"/>
                         </S.emoji__div>
-                        <S.post__submit>Tweet</S.post__submit>
+                        <S.post__submit onClick={(e) => {
+                            e.preventDefault()
+                            add()
+                            }}>Tweet</S.post__submit>
+                            {/* <h1 >oi</h1> */}
                     </S.post__emoji>
                 </S.post__form>
                 <S.main__borderScroll></S.main__borderScroll>
+                
                 <S.main__margin>
+                {tweet.map((item) =>(
                     <S.post__tweet>
                         <S.tweet__figure>
                             <S.tweet__figure__img src="./src/assets/imgs/main/profileDevon.svg" alt="Image of a woman with blonde hair, brown coat and white blouse"/>
                         </S.tweet__figure>
                         <S.tweet__main >
                             <S.tweet__main__user >
-                                <S.name__text >Devon Lane</S.name__text>
+                                <S.name__text>Devon Lane</S.name__text>
                                 <S.name__nick>@johndue</S.name__nick>
                                 <S.name__dot >·</S.name__dot>
-                                <S.name__time >23s</S.name__time>
+                                <S.name__time >{item.time}s</S.name__time>
                             </S.tweet__main__user>
-                            <S.tweet__main__content >Tom is in a big hurry.</S.tweet__main__content>
+                            <S.tweet__main__content >{item.content}</S.tweet__main__content>
                             <S.tweet__main__media >
                                 <S.main__media__img src="./src/assets/imgs/main/astronaut.png" alt="Image of an astronaut in a blue suit in a purple space"/>
                             </S.tweet__main__media>
                             <S.tweet__main__actions >
                                 <S.actions__div >
                                     <S.actionsImg src={comment} alt="Bubble chat icon"/>
-                                    <span>61</span>
+                                    <span>0</span>
                                 </S.actions__div>
                                 <S.actions__div >
                                     <S.actionsImg src={retweet} alt="Retweet related two arrow symbol"/>
-                                    <span>12</span>
+                                    <span>0</span>
                                 </S.actions__div>
                                 <S.actions__div>
                                     <S.actionsImg src={likeDisabled} alt="Red heart symbol"/>
@@ -77,7 +102,7 @@ export default function Main(){
                                 </S.actions__div>
                                 <S.actions__div >
                                     <S.actionsImg src={share} alt="An arrow symbol with a dash below, related to sharing the post"/>
-                                    <span>61</span>
+                                    <span>0</span>
                                 </S.actions__div>
                             </S.tweet__main__actions>
                             <S.tweet__main__show>
@@ -87,11 +112,12 @@ export default function Main(){
                             </S.tweet__main__show>
                         </S.tweet__main>
                     </S.post__tweet>
-                    <S.main__borderScrolll></S.main__borderScrolll>
-                    
-                </S.main__margin>
+                    ))}
+                    <S.main__borderScrolll></S.main__borderScrolll>                                    
+                </S.main__margin>                
                 <S.Margin></S.Margin>
             </S.Post>
         </S.Main>
     )
+    
 }
