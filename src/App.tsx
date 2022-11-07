962609b9e6f8672ca429c2110b48532ef00e7f88
Robert <rjsf06@gmail.com>
2022-11-07 13:50:38 -0300

    :bug: fix: fixing the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import Left from "./components/aside-left/left";
-import * as S from "./appStyle"
 import { ThemeProvider } from "styled-components";
 import light from "./assets/styles/themes/light";
+import dark from "./assets/styles/themes/light";
+import { useState } from "react";
+import Login from "./components/login/login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Reset } from "./assets/styles/reset";
+import Error from "./components/Error/error";
+import Home from "./components/home/home";
+import ProfileDiv from "./components/profileDiv/profileDiv";
 
 export default function App() {
-  return (
-    <ThemeProvider theme={light}>
-      <S.Div>
-        <Left/>
-        {/*  */}
-        {/* <Footer/> */}
-        {/* <Login/> */}
-      </S.Div>
-    </ThemeProvider>
-  )
-}
+  const [theme, setTheme] = useState(light);
 
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
+  return (
+    <BrowserRouter>
+      <Reset/>
+      <ThemeProvider theme={light}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<ProfileDiv />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
