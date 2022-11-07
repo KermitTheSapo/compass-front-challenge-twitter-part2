fe3dd0a4fb445b48b16792dbd276139761b0a5fe
Robert <rjsf06@gmail.com>
2022-11-07 13:53:23 -0300

    :sparkles: feat: changing the title and adding the helmet

--- src/components/Error/error.tsx
+++ src/components/Error/error.tsx
@@ -1,11 +1,15 @@
+import { Helmet } from "react-helmet"
 import { Link } from "react-router-dom"
 import * as S from "./erroStyle"
 
 export default function Error() {
   return (
     <S.ErrorDiv>
+      <Helmet>
+        <title>Error 404</title>
+      </Helmet>
       <S.Title>Hmmm... this page doesn't exist. Try searching for something else.</S.Title>
-      <Link to="/"><S.Btn>Back</S.Btn></Link>
+      <Link to="/home"><S.Btn>Back</S.Btn></Link>
     </S.ErrorDiv>
   )
 }
