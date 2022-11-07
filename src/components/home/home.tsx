8955440f663220ce52dfc15bc96f3314271121e5
Robert <rjsf06@gmail.com>
2022-11-07 20:05:25 -0300

    :sparkles: feat: trying to do login verification and route change

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,7 +5,38 @@ import Footer from "../footer/footer";
 import Main from "../main/main";
 import * as S from "./homeStyle"
 
-export default function Home() {
+// let loginData = localStorage.getItem("name")
+// let passwordData = localStorage.getItem("password")
+// var loginObj = JSON.parse(loginData)
+// var passwordObj = JSON.parse(passwordData)    
+
+// if(loginObj === "admin" && passwordObj === "senha") {
+// } else{
+//     // console.log("asd")
+//     document.location.pathname = "/profile"
+// }
+
+// setTimeout(() => {
+// }, 500)
+
+
+
+// if (!(loginObj === "admin") && !(passwordObj === "senha")){
+//     let userAnswer = confirm("The user is not connected to an account, please go back to the login page to login or enjoy the guest version")
+//     if (userAnswer === true){
+//         document.location.pathname = "/"
+//         userAnswer = false
+//         loginObj = "admin"
+//         passwordObj = "password"
+//     }
+// } else if(!(loginObj === "admin") && !(passwordObj === "senha")){
+//     console.log("senha errada")
+//     // document.location.pathname = "/login"
+// } else{
+//     console.log(passwordObj)
+//     console.log("deu bom")
+// }
+export default function Home() {    
     return(
         <S.Div>
             <Helmet>
