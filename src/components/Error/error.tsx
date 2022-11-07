38c0a1b69d120246df0c933405964d54d87b85c1
Robert <rjsf06@gmail.com>
2022-11-07 09:11:37 -0300

    :sparkles: feat: doing the error structure and btn to home

--- src/components/Error/error.tsx
+++ src/components/Error/error.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import * as S from "./erroStyle"
 
 export default function Error() {
   return (
-    <div>error</div>
+    <S.ErrorDiv>
+      <S.Title>Hmmm... this page doesn't exist. Try searching for something else.</S.Title>
+      <Link to="/"><S.Btn>Back</S.Btn></Link>
+    </S.ErrorDiv>
   )
 }
