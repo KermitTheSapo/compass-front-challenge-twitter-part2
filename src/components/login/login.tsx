639f8e7b118a376a369b60b9fe22557c662e2945
Robert <rjsf06@gmail.com>
2022-11-07 09:17:26 -0300

    :sparkles: feat: doing the login structure and style

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,9 +1,30 @@
-import React from "react"
+import { BrowserRouter} from "react-router-dom"
+import * as S from "./loginStyle"
+
+import twitter from "/src/assets/imgs/asideLeft/white/twitter.svg"
 
 export default function Login(){
+
     return(
-        <div>
-            <h1>oi</h1>
-        </div>
+        <BrowserRouter>
+            <S.SectionLogin>
+                <S.LoginDiv>
+                    <S.Icon src={twitter} alt="" />
+                    <S.Title>Log in to Twitter</S.Title>
+                    <S.InputLogin placeholder="Phone number, email address" type="text" />
+                    <S.InputLogin placeholder="Password" type="password" />
+                    <S.BtnLogin>Log In</S.BtnLogin>
+                    <S.Actions>
+                        <S.LinkActions to="/error">
+                            <S.Paragraph>Forgot password?</S.Paragraph>
+                        </S.LinkActions>
+                        <S.LinkActions to="/error">
+                            <S.Paragraph>Sign up to Twitter</S.Paragraph>
+                        </S.LinkActions>
+                    </S.Actions>
+                </S.LoginDiv>
+            </S.SectionLogin>
+        </BrowserRouter>
     )
 }
+
