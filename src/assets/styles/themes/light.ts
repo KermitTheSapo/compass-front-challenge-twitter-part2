9599948b92253e8042d7c0a6f1eb06bd5215c581
Robert <rjsf06@gmail.com>
2022-11-07 13:51:03 -0300

    :sparkles: feat: doing the dark mode

--- src/assets/styles/themes/light.ts
+++ src/assets/styles/themes/light.ts
@@ -9,3 +9,14 @@ export default {
     text: "#333",
   },
 };
+// --Black: #0F1419;
+//     --Dark1: #17202A;
+//     --Dark2: #1C2733;
+//     --Dark3: #283340;
+//     --Dark4: #3A444C;
+//     --Dark5: #5B7083;
+//     --Dark6: #8899A6;
+//     --Dark7: #EBEEF0;
+//     --Dark8: #F7F9FA;
+//     --PrimaryBlue: #1DA1F2;
+//     --White: #FFFFFF;
