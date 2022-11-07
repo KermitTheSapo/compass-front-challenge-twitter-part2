91742da7deed570d99c1fee4b7a89680d4016d34
Robert <rjsf06@gmail.com>
2022-11-07 09:26:44 -0300

    :lipstick: feat: doing the login style

---
+++ src/components/login/loginStyle.ts
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components";
+
+export const SectionLogin = styled.section`
+    height: 100vh;
+    width: 100vh;
+    background-color: var(--White);
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+`
+
+export const LoginDiv = styled.div`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 476px;
+`
+export const Icon = styled.img`
+    height: 41px;
+    width: 50px;
+`
+export const Title = styled.h1`
+    font-weight: 900;
+    font-size: 2.625rem;
+    color: var(--Black);
+    font-family: 'Roboto', sans-serif;
+`
+export const InputLogin = styled.input`
+    width: 450px;
+    height: 70px;
+    border-radius: 6px;
+    background-color: transparent;
+    border: 1px solid #00000033;
+    font-size: 1.125rem;
+    padding-left: 20px;
+    font-family: 'Roboto', sans-serif;
+`
+
+export const BtnLogin = styled.button`
+    width: 450px;
+    height: 60px;
+    border-radius: 76px;
+    background-color: var(--PrimaryBlue);
+    font-weight: 700;
+    font-size: 1.125rem;
+    color: var(--White);
+    border: none;
+    outline: none;
+    font-family: 'Roboto', sans-serif;
+`
+export const Actions = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 450px;
+`
+export const LinkActions = styled(Link)`
+    text-decoration: none;
+`
+export const Paragraph = styled.p`
+    font-weight: 400;
+    font-size: 1.125rem;
+    color: var(--PrimaryBlue);
+    font-family: 'Roboto', sans-serif;
+`
+
+
