2829ba659cffce1d7fb2fd674b5e3df05289dc43
Robert <rjsf06@gmail.com>
2022-11-08 17:10:28 -0300

    :sparkles: feat: making some adjustments

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,9 +5,9 @@ import Footer from "../footer/footer";
 import Main from "../main/main";
 import * as S from "./homeStyle"
 
-let loginData = localStorage.getItem("name")
-let passwordData = localStorage.getItem("password")
-var loginObj = JSON.parse(loginData)
+let loginData = localStorage.getItem("name") as string
+let passwordData = localStorage.getItem("password") as string
+var loginObj = JSON.parse(loginData) 
 var passwordObj = JSON.parse(passwordData)
 let footerTrue = true
 if (loginObj === "admin" && passwordObj === "admin"){
