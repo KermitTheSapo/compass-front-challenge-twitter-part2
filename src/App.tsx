aa25f7d130dbe08f94ae1eb7ad55b71115f87395
Robert <rjsf06@gmail.com>
2022-11-08 14:04:02 -0300

    :sparkles: feat: doing the darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "styled-components";
+import { DefaultTheme, ThemeProvider } from "styled-components";
 import light from "./assets/styles/themes/light";
 import dark from "./assets/styles/themes/dark";
 import { useState } from "react";
@@ -8,21 +8,21 @@ import { Reset } from "./assets/styles/reset";
 import Error from "./components/Error/error";
 import Home from "./components/home/home";
 import ProfileDiv from "./components/profileDiv/profileDiv";
+import keepState from "./utils/keepState";
 
 export default function App() {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = keepState<DefaultTheme>("theme", light);
 
   const toggleTheme = () => {
     setTheme(theme.title === "light" ? dark : light);
   };
-
   return (
     <BrowserRouter>
+      <ThemeProvider theme={theme}>
       <Reset/>
-      <ThemeProvider theme={light}>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Home toggleTheme={toggleTheme}  />} />
           <Route path="/profile" element={<ProfileDiv />} />
           <Route path="/*" element={<Error />} />
         </Routes>
