9a37227044c0519d4905d7770b6c9e8aa467fe48
Robert <rjsf06@gmail.com>
2022-11-08 07:42:00 -0300

    :sparkles: feat: trying to do the login verification logic

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -9,7 +9,42 @@ import CalendarIcon from "../../assets/imgs/main/CalendarIcon.svg"
 import { useRef, useState } from "react"
 import Post, { PostInterface} from "../post/post"
 
+
+// let loginData = localStorage.getItem("name")
+// let passwordData = localStorage.getItem("password")
+// var loginObj = JSON.parse(loginData)
+// var passwordObj = JSON.parse(passwordData)  
+  
+// if(loginObj === "admin" && passwordObj === "senha") {
+// } else{
+//     // console.log("asd")
+//     document.location.pathname = "/"
+// }
+
+// setTimeout(() => {
+// }, 500)
+
+
+
+// if (!(loginObj === "admin") && !(passwordObj === "senha")){
+//     let userAnswer = confirm("The user is not connected to an account, please go back to the login page to login or enjoy the guest version")
+//     if (userAnswer === true){
+//         document.location.pathname = "/"
+//         userAnswer = false
+//         loginObj = "admin"
+//         passwordObj = "password"
+//     }
+// } else if(!(loginObj === "admin") && !(passwordObj === "senha")){
+//     console.log("senha errada")
+//     // document.location.pathname = "/login"
+// } else{
+//     console.log(passwordObj)
+//     console.log("deu bom")
+// }
+
 export default function Main(){
+
+
   const tweetBtn = document.querySelector("#tweet")
   let now = new Date();
 
