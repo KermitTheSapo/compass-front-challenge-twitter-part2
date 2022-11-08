d515dd77154b7ccc1f5ad7f3c5bdc84e60f9d3b5
Robert <rjsf06@gmail.com>
2022-11-08 07:44:50 -0300

    :sparkles: feat: making the footer appear or disappear if logged in or not

--- src/components/profileDiv/profileDiv.tsx
+++ src/components/profileDiv/profileDiv.tsx
@@ -1,8 +1,23 @@
 import { Helmet } from "react-helmet";
 import Left from "../aside-left/left";
 import Right from "../aside-right/right";
+import Footer from "../footer/footer";
 import Profile from "../profile/profile";
 import * as S from "./profileDivStyle"
+
+let loginData = localStorage.getItem("name")
+let passwordData = localStorage.getItem("password")
+var loginObj = JSON.parse(loginData)
+var passwordObj = JSON.parse(passwordData)  
+console.log(loginObj)
+console.log(passwordObj)
+
+let footerTrue = true
+
+if (loginObj === "admin" && passwordObj === "senha"){
+    footerTrue = false
+}
+
 export default function ProfileDiv(){
     return(
         <S.Div>
@@ -12,6 +27,7 @@ export default function ProfileDiv(){
             <Left/>
             <Profile/>
             <Right/>
+            {!footerTrue ? null : <Footer/> }
         </S.Div>
     )
 }
