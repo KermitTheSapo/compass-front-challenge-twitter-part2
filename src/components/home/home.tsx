6f14a6d3ff85cf16c26efa7f2261bfc81311f193
Robert <rjsf06@gmail.com>
2022-11-08 07:40:37 -0300

    :sparkles: feat: making the footer appear or disappear if logged in or not

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,37 +5,19 @@ import Footer from "../footer/footer";
 import Main from "../main/main";
 import * as S from "./homeStyle"
 
-// let loginData = localStorage.getItem("name")
-// let passwordData = localStorage.getItem("password")
-// var loginObj = JSON.parse(loginData)
-// var passwordObj = JSON.parse(passwordData)    
+let loginData = localStorage.getItem("name")
+let passwordData = localStorage.getItem("password")
+var loginObj = JSON.parse(loginData)
+var passwordObj = JSON.parse(passwordData)  
+console.log(loginObj)
+console.log(passwordObj)
 
-// if(loginObj === "admin" && passwordObj === "senha") {
-// } else{
-//     // console.log("asd")
-//     document.location.pathname = "/profile"
-// }
+let footerTrue = true
 
-// setTimeout(() => {
-// }, 500)
+if (loginObj === "admin" && passwordObj === "senha"){
+    footerTrue = false
+}
 
-
-
-// if (!(loginObj === "admin") && !(passwordObj === "senha")){
-//     let userAnswer = confirm("The user is not connected to an account, please go back to the login page to login or enjoy the guest version")
-//     if (userAnswer === true){
-//         document.location.pathname = "/"
-//         userAnswer = false
-//         loginObj = "admin"
-//         passwordObj = "password"
-//     }
-// } else if(!(loginObj === "admin") && !(passwordObj === "senha")){
-//     console.log("senha errada")
-//     // document.location.pathname = "/login"
-// } else{
-//     console.log(passwordObj)
-//     console.log("deu bom")
-// }
 export default function Home() {    
     return(
         <S.Div>
@@ -45,7 +27,8 @@ export default function Home() {
             <Left/>
             <Main/>
             <Right/>
-            <Footer/>
+            {!footerTrue ? null : <Footer/> }
+            {/* <Footer/> */}
         </S.Div>
     )
 }
