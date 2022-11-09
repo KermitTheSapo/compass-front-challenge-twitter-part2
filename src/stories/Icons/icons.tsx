02b348a3648c2581b2c7be00c63e624d396b45f0
Robert <rjsf06@gmail.com>
2022-11-09 08:37:59 -0300

    :sparkles: feat: allowing icons to change size

--- src/stories/Icons/icons.tsx
+++ src/stories/Icons/icons.tsx
@@ -1,10 +1,11 @@
 import './iconsStyle.css';
 
-interface menuItemProps {
+interface iconProps {
     name?: "twitter" | "house" | "arrow" | "bookmarks" | "calendar" | "comment" | "date" | "dots" | "emoji" | "explore" | "gif" | "graph" | "heart" | "img" | "lists" | "location" | "message" | "mode" | "more" | "notification" | "profile" | "retweet" | "search" | "share" | "twitter";
-    color?: "dark" | "light"
+    color?: "dark" | "light",
+    size?: "small" | "medium" | "large"
 }
 
-export const Icons = ({name, color}:menuItemProps) => (
-    <img src={`./src/stories/assets/icons/${name}-${color}.svg`} alt="asdfasdf" className={`icon`} />
+export const Icons = ({name, color, size}:iconProps) => (
+    <img src={`./src/stories/assets/icons/${name}-${color}.svg`} alt="asdfasdf" className={`icon icon-${size}`} />
 )
