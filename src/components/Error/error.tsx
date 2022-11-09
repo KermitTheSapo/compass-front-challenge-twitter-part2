eaa6d867b7fd60f1c188393f276e38ad108ccfc5
Robert <rjsf06@gmail.com>
2022-11-09 08:36:12 -0300

    :sparkles: feat: checking if you are logged in or not, to redirect to home or login

--- src/components/Error/error.tsx
+++ src/components/Error/error.tsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from "react"
 import { Helmet } from "react-helmet"
-import { Link } from "react-router-dom"
+import {useNavigate } from "react-router-dom"
 import * as S from "./erroStyle"
 
 export default function Error() {
+  let loginData = localStorage.getItem("name") as string
+  let passwordData = localStorage.getItem("password") as string
+  var loginObj = JSON.parse(loginData)
+  var passwordObj = JSON.parse(passwordData)  
+  const navigate = useNavigate()
+  const [login, setLogin] = useState(true)
+  useEffect(() => {
+    if(loginObj === "admin" && passwordObj === "admin") {
+    } else{
+      setLogin(false)
+    }
+  }, [navigate])
   return (
     <S.ErrorDiv>
       <Helmet>
         <title>Error 404</title>
       </Helmet>
       <S.Title>Hmmm... this page doesn't exist. Try searching for something else.</S.Title>
-      <Link to="/home"><S.Btn>Back</S.Btn></Link>
+      <S.Btn onClick={() => {login ? navigate("/home") : navigate("/")}}>Back</S.Btn>
     </S.ErrorDiv>
   )
 }
