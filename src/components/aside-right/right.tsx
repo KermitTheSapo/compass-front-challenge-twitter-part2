741f7f2f31ee1c2fe65c27f3f1c949ebc64f985b
Robert <rjsf06@gmail.com>
2022-11-10 10:57:20 -0300

    :sparkles: feat: change position of news and happening when in profile or home

--- src/components/aside-right/right.tsx
+++ src/components/aside-right/right.tsx
@@ -9,7 +9,7 @@ export default function Right(){
     return(
        <S.asideRight>
             <S.asideRight__input type="text" placeholder="Search Twitter"></S.asideRight__input>
-            <S.Happening>
+            {document.location.pathname === "/home" && <S.Happening>
                 <S.happening__title>What’s happening</S.happening__title>
                 <S.happening__border></S.happening__border>
                 <News alt="image of a white man" hashtag="#covid19" img={news1} theme="COVID19 · Last night" title="England’s Chief Medical Officer says the UK is at the most dangerous time of the pandemic" trending="Trending with"/>
@@ -22,7 +22,7 @@ export default function Right(){
                         <S.showMore__name>Show more</S.showMore__name>
                     </S.showMore__link>
                 </S.happening__showMore>  
-            </S.Happening>
+            </S.Happening>}
             <S.follow>
                 <S.follow__title>Who to follow</S.follow__title>
                 <S.follow__border></S.follow__border>
@@ -60,6 +60,21 @@ export default function Right(){
                     <S.follow__more>Show more</S.follow__more>
                 </S.follow__link>
             </S.follow>
+            {document.location.pathname === "/profile" && 
+            <S.Happening>
+                <S.happening__title>What’s happening</S.happening__title>
+                <S.happening__border></S.happening__border>
+                <News alt="image of a white man" hashtag="#covid19" img={news1} theme="COVID19 · Last night" title="England’s Chief Medical Officer says the UK is at the most dangerous time of the pandemic" trending="Trending with"/>
+                <S.happening__border></S.happening__border>
+                <News alt="image of a white man" hashtag="#trump" img={news2} theme="US news · 4h ago" title="Parler may go offline following suspensions by Amazon, Apple and Google" trending="Trending with"/>
+                <S.happening__border></S.happening__border>
+                <News alt="image of a white man" hashtag="#sport" img={news3} theme="India · 1h ago" title="India vs Australia: India hold on to earn a draw on Day 5 in Sydney Test" trending="Trending with"/>
+                <S.happening__showMore>
+                    <S.showMore__link href="/error_404">
+                        <S.showMore__name>Show more</S.showMore__name>
+                    </S.showMore__link>
+                </S.happening__showMore>  
+            </S.Happening>}
             <S.follow__terms>Terms of Service Privacy Policy Cookie Policy
         Ads info More © 2021 Twitter, Inc.</S.follow__terms>
        </S.asideRight>
