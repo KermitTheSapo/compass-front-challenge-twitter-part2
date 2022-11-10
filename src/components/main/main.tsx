4ae3192c468ab0a35a930e3a8311b0c7b203b00f
Robert <rjsf06@gmail.com>
2022-11-10 10:59:42 -0300

    :sparkles: feat: putting inside functions, saving post to localstorage

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -7,28 +7,30 @@ import graphIcon from "../../assets/imgs/main/graphIcon.svg"
 import emojiIcon from "../../assets/imgs/main/emojiIcon.svg"
 import CalendarIcon from "../../assets/imgs/main/CalendarIcon.svg"
 import { useEffect, useRef, useState } from "react"
-import Post, { PostInterface} from "../post/post"
+import Post, { PostInterface } from "../post/post"
 import { useNavigate } from "react-router-dom"
 
 interface Props {
   toggleTheme(): void;
 }
 
-export default function Main({ toggleTheme }: Props){
+export default function Main({ toggleTheme }: Props) {
   let loginData = localStorage.getItem("name") as string
   let passwordData = localStorage.getItem("password") as string
   var loginObj = JSON.parse(loginData)
-  var passwordObj = JSON.parse(passwordData)  
+  var passwordObj = JSON.parse(passwordData)
   const navigate = useNavigate()
   const [isGuess, setIsGuess] = useState(false)
+  let postData = localStorage.getItem("post") as string
+  var postObj = JSON.parse(postData)
   useEffect(() => {
-    if(loginObj === "admin" && passwordObj === "admin") {
-    } else{
+    if (loginObj === "admin" && passwordObj === "admin") {
+    } else {
       let userAnswer = confirm("The user is not connected to an account, please go back to the login page to login or enjoy the guest version")
-      if (userAnswer === true){
+      if (userAnswer === true) {
         navigate("/")
       }
-      else{
+      else {
         setIsGuess(true)
       }
     }
@@ -52,10 +54,10 @@ export default function Main({ toggleTheme }: Props){
     }
   ]);
   const [inputValue, setInputValue] = useState("")
-  const [imgValue, setImgValue] = useState("")  
+  const [imgValue, setImgValue] = useState("")
   const inputElement = useRef();
 
-  const createPost = () =>{    
+  const createPost = () => {
     const post: PostInterface = {
       time: `${now.getHours()}h${now.getMinutes()}m`,
       commentValue: 0,
@@ -73,28 +75,66 @@ export default function Main({ toggleTheme }: Props){
     setImgValue("")
   }
 
+  const checkLogin = () => {
+    if (loginObj === "admin" && passwordObj === "admin") {
+      navigate("/error")
+    } else {
+      alert("You are in guest mode, you can't post")
+    }
+  }
+
+  const checkLoginAndCreatePost = (e: Event) => {
+    e.preventDefault()
+    if (isGuess === true) {
+      alert("You are in guest mode, you can't post")
+      return;
+    } else {
+      if (inputValue.length > 0 || imgValue.length > 0) {
+        createPost()
+        return;
+      } else {
+        alert("You need to write something or add an image")
+      }
+    }
+  }
+
+  const changeOpacity = (event) => {
+    setInputValue(event.target.value)
+    if (event.target.value.length > 0) {
+      tweetBtn?.setAttribute("style", "opacity: 1")
+    } else {
+      tweetBtn?.setAttribute("style", "opacity: 0.5")
+    }
+  }
+
+  const getImage = (event) => {
+    if (event.target.value.length > 0) {
+      tweetBtn?.setAttribute("style", "opacity: 1")
+    } else {
+      tweetBtn?.setAttribute("style", "opacity: 0.5")
+    }
+    const files = event.target.files[0]
+    console.log(files)
+    const reader = new FileReader()
+    reader.onload = (eventReader) => {
+      setImgValue(eventReader.target?.result)
+    }
+    reader.readAsDataURL(files)
+    setTimeout(() => {
+      event.target.value = ""
+    }, 500)
+  }
+
   return (
     <S.Main>
       <S.main__header>
         <S.main__header__title>Home</S.main__header__title>
-        <S.main__header__img src={darkLightMode} alt="Three blue stars" onClick={() => toggleTheme()}/>
+        <S.main__header__img src={darkLightMode} alt="Three blue stars" onClick={() => toggleTheme()} />
       </S.main__header>
       <S.main__borderHeader></S.main__borderHeader>
       <S.Post>
         <S.post__form
-          onSubmit={(e) => {e.preventDefault()
-            if (isGuess === true){
-              alert("You are in guest mode, you can't post")
-              return;
-            } else{
-              if (inputValue.length > 0 || imgValue.length > 0){
-                createPost()
-                return;
-              } else{
-                alert("You need to write something or add an image")
-              }
-            }
-        }}
+          onSubmit={(e) => { checkLoginAndCreatePost(e) }}
           method="post"
         >
           <S.post__input>
@@ -102,55 +142,18 @@ export default function Main({ toggleTheme }: Props){
               src={profile}
               alt="A profile picture of a person with a black shirt and hair on their face"
             />
-            <S.input__text
-              id="input"
-              ref={inputElement}
-              type="text"
-              placeholder="What's happening?"
-              minLength={2}
-              maxLength={380}
-              value={inputValue}
-              onChange={(event) => {
-                setInputValue(event.target.value)
-                if (event.target.value.length > 0){
-                  tweetBtn?.setAttribute("style", "opacity: 1")
-                } else {
-                  tweetBtn?.setAttribute("style", "opacity: 0.5")
-                }
-              }}
-            />
+            <S.input__text id="input" ref={inputElement} type="text" placeholder="What's happening?" minLength={2} maxLength={380} value={inputValue} onChange={(event) => { changeOpacity(event) }} />
           </S.post__input>
           <S.post__emoji>
             <S.emoji__div>
               <S.label htmlFor="upload">
-                <S.LabelImg
-                  src={imgIcon}
-                  alt="Blue icon of a landscape and sun related to image upload"
-                />
+                <S.LabelImg src={imgIcon} alt="Blue icon of a landscape and sun related to image upload" />
               </S.label>
-              <S.upload__img
-                id="upload"
-                type="file"
-                name="file"
-                accept="image/png, image/jpeg"
-                multiple
-                onChange={(event) => {
-                  const files = event.target.files[0]
-                  console.log(files)
-                  const reader = new FileReader()
-                  reader.onload = (eventReader) => {
-                    setImgValue(eventReader.target?.result)
-                  }
-                  reader.readAsDataURL(files)
-                  setTimeout(() => {
-                    event.target.value = ""
-                  }, 500)
-                }}
-              />
-              <S.icons src={gifIcon} alt="A square with 'GIF' inside" />
-              <S.icons src={graphIcon} alt="A chart icon" />
-              <S.icons src={emojiIcon} alt="Happy face icon" />
-              <S.icons src={CalendarIcon} alt="Calendar icon" />
+              <S.upload__img id="upload" type="file" name="file" accept="image/png, image/jpeg" multiple onChange={(event) => { getImage(event) }} />
+              <S.icons onClick={() => { checkLogin() }} src={gifIcon} alt="A square with 'GIF' inside" />
+              <S.icons onClick={() => { checkLogin() }} src={graphIcon} alt="A chart icon" />
+              <S.icons onClick={() => { checkLogin() }} src={emojiIcon} alt="Happy face icon" />
+              <S.icons onClick={() => { checkLogin() }} src={CalendarIcon} alt="Calendar icon" />
             </S.emoji__div>
             <S.post__submit id="tweet" >Tweet</S.post__submit>
           </S.post__emoji>
@@ -158,7 +161,8 @@ export default function Main({ toggleTheme }: Props){
         <S.main__borderScroll></S.main__borderScroll>
 
         <S.main__margin>
-          {dataTweet.map((data) => <Post data={data}/>)}        
+          {/* {dataTweet.map((data) => <Post data={data} />)} */}
+          {postObj && postObj.map((data) => <Post data={data} />)}
           <S.main__borderScrolll></S.main__borderScrolll>
         </S.main__margin>
         <S.Margin></S.Margin>
