7b0be2ade5dab9427d7418b36169efbeadc4771e
Robert <rjsf06@gmail.com>
2022-11-10 11:01:45 -0300

    :sparkles: feat: transforming into fractions, checking the button if you are logged in, making comments appear and disappear

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,20 +1,20 @@
 import * as S from "./postStyle";
 //@ts-nocheck 
 
-export interface PostInterface{
-    name: string;
-    userName: string;
-    time: string;
-    content: string;
-    imgs?: string;
-    commentValue: number;
-    retweetValue: number;
-    likeValue: number;
-    shareValue: number;
+export interface PostInterface {
+  name: string;
+  userName: string;
+  time: string;
+  content: string;
+  imgs?: string;
+  commentValue: number;
+  retweetValue: number;
+  likeValue: number;
+  shareValue: number;
 }
 
 export type PostProps = {
-    data:PostInterface
+  data: PostInterface
 }
 
 import comment from "../../assets/imgs/main/comment.svg"
@@ -25,7 +25,11 @@ import share from "../../assets/imgs/main/share.svg"
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 export default function Post(props: PostProps) {
-  
+  let loginData = localStorage.getItem("name") as string
+  let passwordData = localStorage.getItem("password") as string
+  var loginObj = JSON.parse(loginData)
+  var passwordObj = JSON.parse(passwordData)
+  const [showThisThread, setShowThisThread] = useState(true)
 
   const navigate = useNavigate()
   let now = new Date();
@@ -35,13 +39,46 @@ export default function Post(props: PostProps) {
   const [commentValue, setCommentValue] = useState([{
     name: ""
   }])
+
+  const createComment = () => {
+    if (loginObj === "admin" && passwordObj === "admin") {
+      if (inputValue.length > 0) {
+        inputValue.length > 1 ? setCommentValue([...commentValue, { name: inputValue }]) : null;
+        setInputValue("")
+        return;
+      } else {
+        alert("You need to write something!")
+      }
+    } else {
+      alert("You are in guest mode, you can't post")
+    }
+  }
+  const canReplay = () => {
+    if (loginObj === "admin" && passwordObj === "admin") {
+      commentDiv ? setCommentDiv(false) : setCommentDiv(true); 
+    } else {
+      alert("You are in guest mode, you can't post")
+    }
+  }
+
+  const changeBtnOpacity = (e) => {
+    const btnReplyOpacity = document.querySelector("#btnReplyOpacity") as HTMLElement
+    setInputValue(e.target.value);
+    e.target.value.length > 0 ? btnReplyOpacity.style.opacity = "1.0" : btnReplyOpacity.style.opacity = "0.5"
+  }
+
+  const makeLikeCount = () => {
+    if (loginObj === "admin" && passwordObj === "admin") {
+      countLike === 0 ? setCountLike(1) : setCountLike(0)
+    } else {
+      alert("You are in guest mode, you can't post")
+    }
+  }
+
   return (
     <S.post__tweet>
       <S.tweet__figure>
-        <S.tweet__figure__img
-          src="./src/assets/imgs/main/profile.png"
-          alt="Image of a woman with blonde hair, brown coat and white blouse"
-        />
+        <S.tweet__figure__img src="./src/assets/imgs/main/profile.png" alt="Image of a woman with blonde hair, brown coat and white blouse" />
       </S.tweet__figure>
       <S.tweet__main>
         <S.tweet__main__user>
@@ -59,47 +96,34 @@ export default function Post(props: PostProps) {
           /> : ""}
         </S.tweet__main__media>
         <S.tweet__main__actions>
-          <S.actions__div onClick={() => {commentDiv ? setCommentDiv(false) : setCommentDiv(true)}}>
+          <S.actions__div onClick={() => { canReplay() }}>
             <S.actionsImg src={comment} alt="Bubble chat icon" />
-            <S.SpanNumber>{commentValue.length > 1 ? props.data.commentValue + commentValue.length - 1 : props.data.commentValue }</S.SpanNumber>
+            <S.SpanNumber>{commentValue.length > 1 ? props.data.commentValue + commentValue.length - 1 : props.data.commentValue}</S.SpanNumber>
           </S.actions__div>
           <S.actions__div>
-            <S.actionsImg
-              src={retweet}
-              alt="Retweet related two arrow symbol"
-            />
+            <S.actionsImg src={retweet} alt="Retweet related two arrow symbol" />
             <S.SpanNumber>{props.data.retweetValue}</S.SpanNumber>
           </S.actions__div>
-          <S.actions__div onClick={() => {countLike === 0 ? setCountLike(1) : setCountLike(0) }}>
+          <S.actions__div onClick={() => { makeLikeCount() }}>
             <S.actionsImg src={countLike === 0 ? likeDisabled : likeActive} alt="Red heart symbol" />
             <S.SpanNumber>{props.data.likeValue + countLike}</S.SpanNumber>
           </S.actions__div>
           <S.actions__div>
-            <S.actionsImg
-              src={share}
-              alt="An arrow symbol with a dash below, related to sharing the post"
-            />
+            <S.actionsImg src={share} alt="An arrow symbol with a dash below, related to sharing the post" />
             <S.SpanNumber>{props.data.shareValue}</S.SpanNumber>
           </S.actions__div>
         </S.tweet__main__actions>
-          {commentDiv && 
-          <S.CommentBlock onSubmit={(e) => {e.preventDefault()}}>
-            <S.InputComment value={inputValue} onChange={(e) => {
-              const btnReplyOpacity = document.querySelector("#btnReplyOpacity") as HTMLElement
-              setInputValue(e.target.value);
-              e.target.value.length > 0 ? btnReplyOpacity.style.opacity = "1.0" : btnReplyOpacity.style.opacity = "0.5"
-              }} type="text" />
-            <S.ButtonReply id="btnReplyOpacity"  onClick={() => {
-              inputValue.length > 1 ? setCommentValue([...commentValue, {name: inputValue}]) : null;
-              setInputValue("")
-              }}>Reply</S.ButtonReply>
+        {commentDiv &&
+          <S.CommentBlock onSubmit={(e) => { e.preventDefault() }}>
+            <S.InputComment placeholder="Tweet your reply" value={inputValue} onChange={(e) => { changeBtnOpacity(e) }} type="text" />
+            <S.ButtonReply id="btnReplyOpacity" onClick={() => { createComment() }}>Reply</S.ButtonReply>
           </S.CommentBlock>}
-          {commentValue.length > 1 && commentValue.map((item) => (
-            item.name.length < 1 ? null :
+        {showThisThread && commentValue.length > 1 && commentValue.map((item) => (
+          item.name.length < 1 ? null :
             <S.CommentPost>
-              <S.tweet__figure__img src="./src/assets/imgs/main/profileDevon.svg" alt="Image of a woman with blonde hair, brown coat and white blouse"/>
+              <S.tweet__figure__img src="./src/assets/imgs/main/profileDevon.svg" alt="Image of a woman with blonde hair, brown coat and white blouse" />
               <S.ContentDiv>
-              <S.tweet__main__user>
+                <S.tweet__main__user>
                   <S.name__text>Devon Lane</S.name__text>
                   <S.name__nick>@marcelosalomao</S.name__nick>
                   <S.name__dot>·</S.name__dot>
@@ -108,10 +132,10 @@ export default function Post(props: PostProps) {
                 <S.ParagraphComment>{item.name}</S.ParagraphComment>
               </S.ContentDiv>
             </S.CommentPost>
-          ) )}
+        ))}
         <S.tweet__main__show>
-          <S.show__link onClick={() => navigate("/error")}>
-            <S.show__paragraph>Show this thread</S.show__paragraph>
+          <S.show__link onClick={() => showThisThread === true ? setShowThisThread(false) : setShowThisThread(true)}>
+            {showThisThread === true ? <S.show__paragraph>Hide this thread</S.show__paragraph> : <S.show__paragraph>Show this thread</S.show__paragraph>}
           </S.show__link>
         </S.tweet__main__show>
       </S.tweet__main>
