6f74c65fbbd90c3da9a1d9c6e1654bee0d0286ac
Robert <rjsf06@gmail.com>
2022-11-10 10:55:31 -0300

    :sparkles: feat: adding more content in error 404

--- src/components/Error/error.tsx
+++ src/components/Error/error.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react"
 import { Helmet } from "react-helmet"
-import {useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import * as S from "./erroStyle"
 
 export default function Error() {
   let loginData = localStorage.getItem("name") as string
   let passwordData = localStorage.getItem("password") as string
   var loginObj = JSON.parse(loginData)
-  var passwordObj = JSON.parse(passwordData)  
+  var passwordObj = JSON.parse(passwordData)
   const navigate = useNavigate()
   const [login, setLogin] = useState(true)
   useEffect(() => {
-    if(loginObj === "admin" && passwordObj === "admin") {
-    } else{
+    if (loginObj === "admin" && passwordObj === "admin") {
+    } else {
       setLogin(false)
     }
   }, [navigate])
@@ -21,8 +21,9 @@ export default function Error() {
       <Helmet>
         <title>Error 404</title>
       </Helmet>
+      <S.Title>Error 404</S.Title>
       <S.Title>Hmmm... this page doesn't exist. Try searching for something else.</S.Title>
-      <S.Btn onClick={() => {login ? navigate("/home") : navigate("/")}}>Back</S.Btn>
+      <S.Btn onClick={() => { login ? navigate("/home") : navigate("/") }}>Back</S.Btn>
     </S.ErrorDiv>
   )
 }
